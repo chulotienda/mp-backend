47d67917eeeef6c9586a07b56840fffd84c3aca0
chulotienda <chulotienda26@gmail.com>
2026-03-07 23:24:23 -0300

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
   try {
 
-    const { payment_id } = req.body;
+    const payment_id = req.body.payment_id || req.body.paymentId;
 
     if (!payment_id) {
       return res.status(400).json({ error: "payment_id requerido" });
